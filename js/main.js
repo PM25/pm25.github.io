7dd363c7f714da89bb8a1c87dc1f8741c940a70f
einz <jason199786109@gmail.com>
2018-06-18 14:14:12 +0800

    only left mouse click will work on side navigation button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 $(document).ready(function()
 {
-    $(".sidenav-btn").mousedown(function () {
-        ripple_div = $("<div></div>");
-        ripple_div.addClass("ripple-effect");
-        $(this).append(ripple_div);
+    $(".sidenav-btn").mousedown(function (event) {
+        if(event.which === 1){
+            ripple_div = $("<div></div>");
+            ripple_div.addClass("ripple-effect");
+            $(this).append(ripple_div);
+        }
     });
-    $(".sidenav-btn").mouseup(function(){
-        window.setTimeout(function(){
-            ripple_div.remove();
-            $("#side-nav").toggleClass("show-sidenav");
-        }, 100);
+    $(".sidenav-btn").mouseup(function(event){
+        if(event.which === 1){
+            window.setTimeout(function(){
+                ripple_div.remove();
+                $("#side-nav").toggleClass("show-sidenav");
+            }, 100);
+        }
     });
 
     $("main").click(function(){
