7c5e02d33ed7caa391fbf1d0cf5ced62ddf318b3
Einz <jason199786109@gmail.com>
2018-11-20 00:08:22 +0800

    Add Google analytics

--- js/common.js
+++ js/common.js
@@ -1,7 +1,8 @@
 $(document).ready(function()
 {
     function checkFinish(left){
-        if(!left) $.getScript("https://pm25.github.io/js/main.js");
+        if(!left) $.getScript("https://pm25.github.io/js/main.js")
+            .fail(function(){ checkFinish(0); }); // If fail loading the script, then try again!
     }
 
     var includes = $("[data-include]");
