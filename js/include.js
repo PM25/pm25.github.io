c3742cba7e8b9a5e5bc4317f29cfb70105375753
Einz <jason199786109@gmail.com>
2018-11-19 17:44:39 +0800

    Testing jQuery ajax - load()

---
+++ js/include.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+    var includes = $("[data-include]");
+    jQuery.each(includes, function(){
+      var file = "html/" + $(this).data("include") + ".html";
+      $(this).load(file);
+    });
+});
