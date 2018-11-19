b50dd7f7bd8b585bd52521ee88b4ad99d54c6925
Einz <jason199786109@gmail.com>
2018-11-19 18:06:01 +0800

    include home-nav bar

--- js/include.js
+++ js/include.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {
     var includes = $("[data-include]");
     jQuery.each(includes, function(){
-      var file = "html/" + $(this).data("include") + ".html";
+      var file = "https://pm25.github.io/html/" + $(this).data("include") + ".html";
       $(this).load(file);
     });
 });
