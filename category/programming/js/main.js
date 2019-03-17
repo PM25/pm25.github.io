182a504d9181839f819d1cebabcb640987a77982
Einz <jason199786109@gmail.com>
2019-03-17 15:42:17 +0800

    Get the link of article

--- category/programming/js/main.js
+++ category/programming/js/main.js
@@ -1,9 +1,12 @@
 $(function(){
-    var $article_blocks = $("#articles").children("div")
-    for(var idx=0; idx < $article_blocks.length; ++idx){
-        $article_blocks.eq(idx).css("display", "block");
-        $article_blocks.eq(idx).children(".article-title").html("Test-Title");
-        $article_blocks.eq(idx).children(".date").html("2019-03-16");
-        $article_blocks.eq(idx).children(".preview").html("bla bla bla ...");
-    }
+    $.getJSON("https://pm25.github.io/category/programming/links.json", function(data){
+        var $article_blocks = $("#articles").children("div")
+        for(var idx=0; idx < $article_blocks.length && data["links"].length; ++idx) {
+            $article_blocks.eq(idx).css("display", "block");
+            $article_blocks.eq(idx).children(".article-title").attr("href", data["links"][idx])
+            $article_blocks.eq(idx).children(".article-title").html("Test-Title");
+            $article_blocks.eq(idx).children(".date").html("2019-03-16");
+            $article_blocks.eq(idx).children(".preview").html("bla bla bla ...");
+        }
+    });
 });
