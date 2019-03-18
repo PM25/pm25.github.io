b25eec355911effba30f8bd30bb204dec57f85fc
Einz <jason199786109@gmail.com>
2019-03-18 15:36:07 +0800

    Implement control panel

--- category/programming/js/main.js
+++ category/programming/js/main.js
@@ -2,27 +2,42 @@ baseLink = "https://pm25.github.io/category/programming/articles/"
 
 $(function(){
     $.getJSON(baseLink + "links.json", function(data){
-        var $article_blocks = $("#articles").children("div"); // Elements of article block
+        var $article_blocks = $("#article-blocks").children("div"); // Elements of article block
+        var $control_panel = $("#articles .control");
         var articles_data = data["articles"]; // Data related to article (eg: title, date ...)
-        var blog = new Blog($article_blocks, articles_data);
-
-        blog.load_page();
-        blog.next_page();
-        blog.next_page();
+        
+        var blog = new Blog($article_blocks, $control_panel, articles_data);
+        blog.init();
     });
 });
 
 
 // Blog Object
-var Blog = function(article_blocks, articles_data)
+var Blog = function($article_blocks, $control_panel, articles_data)
 {
-    this.$article_blocks = article_blocks;
+    this.$article_blocks = $article_blocks;
+    this.$control_panel = $control_panel;
     this.articles_data = articles_data;
     this.base_idx = 0;
     this.per_page_count = this.$article_blocks.length;
     this.total_count = this.articles_data.length;
     this.page_idx = 0;
     this.max_page = Math.ceil(this.total_count / this.per_page_count);
+
+    // Behavior
+    var self = this;
+    this.$control_panel.find(".prev").click(function(){
+        self.prev_page();
+    });
+    this.$control_panel.find(".next").click(function(){
+        self.next_page();
+    });
+}
+
+// Initialize
+Blog.prototype.init = function()
+{
+    this.update_page();
 }
 
 // Load the page base on {base_idx}
@@ -52,6 +67,8 @@ Blog.prototype.next_page = function()
     } else {
         this.page_idx = this.max_page - 1;
     }
+
+    this.update_page();
 }
 
 // Change to previous page
@@ -64,6 +81,8 @@ Blog.prototype.prev_page = function()
     } else {
         this.page_idx = this.max_page - 1;
     }
+
+    this.update_page();
 }
 
 // Clear the page
@@ -72,4 +91,13 @@ Blog.prototype.clear_page = function()
     for(var idx=0; idx < this.per_page_count; ++idx) {
         this.$article_blocks.eq(idx).css("display", "none");
     }
+}
+
+// Update page information & reload the page
+Blog.prototype.update_page = function()
+{
+    this.load_page();
+
+    var page_info = "Page " + (this.page_idx+1) + " of " + this.max_page;
+    this.$control_panel.find(".info").html(page_info);
 }
