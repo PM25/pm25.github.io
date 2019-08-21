0e3eb78be32931f5183b48d9e880c0eae256d256
PM <jason199786109@gmail.com>
2019-08-21 13:34:52 -0500

    Fixed bugs.

--- js/articles-master.js
+++ js/articles-master.js
@@ -16,9 +16,28 @@ require.config({
 });
  
  
-require(["jquery", "marked", "simplebar", "common", "gtm", "ga", "articles"], function($, marked){
-    window.marked = marked;
+require(["jquery", "marked", "simplebar", "common", "gtm", "ga"], function($, marked){
+    // Load markdown content.
+    $.get( get_md_url(), function( md ) {
+        var html = marked(md);
+
+        $("#blog").html(html);
+        console.log( "Blog content was Loaded." );
+    });
+
     // View Counter
     $.getJSON("https://plusmore-view-counter.herokuapp.com", function(data){});
     console.log("all js files loaded!");
 });
+
+// Find corresponding markdown file.
+function get_md_url()
+{
+    var curr_url = $(location).attr('href');
+    var html_filename = curr_url.split("/").pop();
+    var filename = html_filename.split('.', 1);
+    var md_filename = filename + ".md";
+    var md_url = "https://pm25.github.io/articles/md/" + md_filename;
+    
+    return md_url;
+}
