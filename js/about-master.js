3c8e2131dbdfac283889497cff073ce55f554538
PM <jason199786109@gmail.com>
2019-08-26 19:34:49 -0500

    Fixed bug.

--- js/about-master.js
+++ js/about-master.js
@@ -8,26 +8,66 @@ require.config({
     shim: {
         "common": {
             deps:["jquery"]
-        },
-        "about": {
-            deps: ["jquery", "firebase"]
         }
     }
 });
  
  
-require(["jquery", "firebase", "simplebar", "common", "gtm", "ga", "about"], function($, firebase){
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyC_PsGVyb044JCGNuO2AYEfNBStYE559xQ",
-        authDomain: "plusmore-website.firebaseapp.com",
-        databaseURL: "https://plusmore-website.firebaseio.com",
-        projectId: "plusmore-website",
-        storageBucket: "plusmore-website.appspot.com",
-        messagingSenderId: "971689883643"
-    }; firebase.initializeApp(config);
+require(["jquery", "firebase", "simplebar", "common", "gtm", "ga"], function($, firebase){
+    $(function(){
+        // View Counter
+        $.getJSON("https://plusmore-view-counter.herokuapp.com", function(data){});
+        
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyC_PsGVyb044JCGNuO2AYEfNBStYE559xQ",
+            authDomain: "plusmore-website.firebaseapp.com",
+            databaseURL: "https://plusmore-website.firebaseio.com",
+            projectId: "plusmore-website",
+            storageBucket: "plusmore-website.appspot.com",
+            messagingSenderId: "971689883643"
+        }; firebase.initializeApp(config);
+        main(firebase.database());
 
-    // View Counter
-    $.getJSON("https://plusmore-view-counter.herokuapp.com", function(data){});
-    console.log("all js files loaded!");
+        console.log("all js files loaded!");
+    });
 });
+
+
+// Main function
+function main(db)
+{
+    // Show client's IP data
+    $.getJSON("https://plusmore-view-counter.herokuapp.com/", function(data) {
+        clientIP = data["ip"].replace(/\./g, '-');
+        
+        $("#site-statics").find(".ip").text(data["ip"]);
+        $("#site-statics").find(".city").text(data["city"]);
+        db.ref("/ipData/ip/" + clientIP).once("value", (snapshot) => {
+            $("#site-statics").find(".view").text(snapshot.val()["count"]);
+        });
+    });
+    
+    // Show site's overview data
+    db.ref("/ipData/ip/total").on("value", (snapshot) => {
+        $("#site-statics").find(".total-ip").text(snapshot.val()["ip"]);
+        $("#site-statics").find(".total-view").text(snapshot.val()["view"]);
+    });
+
+    // Show how long since the first time site started
+    var createdTime = new Date(2018, 5, 24);
+    $("#site-statics").find(".running-time").text(msToTime(new Date - createdTime));
+    setInterval(function(){
+        $("#site-statics").find(".running-time").text(msToTime(new Date - createdTime));
+    }, 1000);
+}
+
+function msToTime(duration) 
+{
+    var sec = Math.floor((duration / 1000) % 60);
+    var min = Math.floor((duration / 60000) % 60);
+    var hr = Math.floor((duration / 3600000) % 24);
+    var day = Math.floor(duration / 86400000);
+    
+    return day + "天 " + hr + "小時 " + min + "分鐘 " + sec + "秒";
+}
