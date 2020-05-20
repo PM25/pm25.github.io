5998a24722bd3318014073106e3c35a77c7d5e6c
PM <pyhuang97@gmail.com>
2020-05-21 00:05:33 +0800

    reimplement jQuery code with native javascript.

--- js/common.js
+++ js/common.js
@@ -1,23 +1,60 @@
-$(document).ready(function()
-{
-    var includes = $("[data-include]");
-    var count = includes.length;
-    jQuery.each(includes, function(){
-        var file = "https://pm25.github.io/html/" + $(this).data("include") + ".html";
+window.addEventListener("load", load_html);
+
+function load_html() {
+    let includes = document.querySelectorAll("[data-include]"),
+        count = includes.length;
+
+    includes.forEach((include) => {
+        let value = include.getAttribute("data-include"),
+            fpath = "/html/" + value + ".html";
         // After loading all externel HTML files then load common-main.js
-        $(this).load(file, checkFinish(--count));
+        get_file(fpath, (data) => {
+            include.innerHTML = data;
+
+            if (--count == 0) {
+                main();
+            }
+        });
     });
-});
+}
 
+function get_file(fpath, callback) {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", fpath, true);
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status == "200") {
+            callback(this.responseText);
+        }
+    };
+    xmlhttp.send();
+}
 
-function checkFinish(left){
-    if(!left) $.getScript("https://pm25.github.io/js/common-main.js")
-        .fail(function(){ // If fail loading the script, then try again! 
-            setTimeout(function(){
-                if(window.console) console.log("*Error: Failed loading common-main.js");
-                checkFinish(0);
-            }, 100); 
-        }).done(function(){
-            if(window.console) console.log("common-main.js loaded!");
-        }); 
+function main() {
+    let sidenav = document.querySelector("#side-nav");
+    let sidenav_btns = document.querySelectorAll(".sidenav-btn");
+
+    let ripple_effect = document.createElement("div");
+    ripple_effect.classList.add("ripple-effect");
+
+    sidenav_btns.forEach(function (sidenav_btn) {
+        sidenav_btn.addEventListener("mousedown", function () {
+            sidenav_btn.appendChild(ripple_effect);
+        });
+        sidenav_btn.addEventListener("mouseup", function () {
+            setTimeout(function () {
+                sidenav_btn.removeChild(ripple_effect);
+                sidenav.classList.toggle("show-sidenav");
+
+                // Fixed problem that cursor will change to default after sidenav-btn clicked.
+                sidenav.classList.add("btn");
+                setTimeout(function () {
+                    sidenav.classList.remove("btn");
+                }, 500);
+            }, 150);
+        });
+    });
+
+    document.querySelector("main").addEventListener("click", function () {
+        sidenav.classList.remove("show-sidenav");
+    });
 }
--- js/master.js
+++ js/master.js
@@ -1,19 +1,10 @@
 require.config({
-    paths : {
-        "jquery" : "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min",
-        "simplebar" : "https://unpkg.com/simplebar@latest/dist/simplebar",
-        "gtm" : "https://www.googletagmanager.com/gtag/js?id=UA-129342449-2"
+    paths: {
+        simplebar: "https://unpkg.com/simplebar@latest/dist/simplebar",
+        gtm: "https://www.googletagmanager.com/gtag/js?id=UA-129342449-2",
     },
-    shim: {
-        'common':{
-            deps:["jquery"]
-        }
-    }
 });
- 
- 
-require(["jquery", "simplebar", "common", "gtm", "ga"], function(){
-    // View Counter
-    $.getJSON("https://plusmore-view-counter.herokuapp.com", function(data){});
+
+require(["simplebar", "common", "gtm", "ga"], function () {
     console.log("all js files loaded!");
 });
