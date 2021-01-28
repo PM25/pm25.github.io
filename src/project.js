00bf905da0a0d9937031b5d329be4729e623f4ca
PY <pyhuang97@gmail.com>
2021-01-28 14:35:57 +0800

    update components & GA support.

--- src/project.js
+++ src/project.js
@@ -1,8 +1,23 @@
 import React, { PureComponent } from "react";
 import "./project.css";
 import sourceData from "./source/list.json";
+import { useGA } from "./components/src/google-analytics";
 
-export default class Project extends PureComponent {
+export default function Project() {
+    useGA();
+    return (
+        <div id="project" className="main">
+            <div className="header">
+                <a href=".">Project</a>
+            </div>
+            <div className="projects-list">
+                <ProjectBlocks />
+            </div>
+        </div>
+    );
+}
+
+class ProjectBlocks extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
@@ -11,28 +26,14 @@ export default class Project extends PureComponent {
     }
 
     render() {
-        return (
-            <div id="project" className="main">
-                <div className="header">
-                    <a href=".">Project</a>
-                </div>
-                <div className="projects-list">
-                    {this.state.sourceData.map((state, key) => {
-                        return (
-                            <ProjectBlock
-                                id={key}
-                                name={state.name}
-                                url={state.url}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
-        );
+        return this.state.sourceData.map((state, key) => {
+            return <ProjectBlock id={key} name={state.name} url={state.url} />;
+        });
     }
 }
 
 function ProjectBlock(props) {
+    useGA();
     return (
         <a href={props.url} target="_blank" rel="noreferrer">
             {props.name}
