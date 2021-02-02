17af5bd6db1e1be3ba76c46b6d2c238361e6e63a
PY <pyhuang97@gmail.com>
2021-02-02 12:18:40 +0800

    automatic generate sitemap.

---
+++ sitemap-generator.js
@@ -0,0 +1,51 @@
+require("babel-register")({
+    presets: ["es2015", "react"],
+});
+
+const fetch = require("node-fetch").default;
+const router = require("./sitemap-routes").default;
+const Sitemap = require("react-router-sitemap").default;
+const projectList = require("./src/project/src/source/list.json");
+
+async function generateSitemap() {
+    fetch("https://pm25.github.io/my-articles/list.json")
+        .then((res) => res.json())
+        .then(
+            (articleList) => {
+                let articleIdMap = articleList.map((article, key) => {
+                    return { id: article.name.replace(/ /g, " ") };
+                });
+
+                let projectIdMap = projectList
+                    .filter((project) => {
+                        let splittedPath = project.url.split("/");
+                        let rootPath = splittedPath[splittedPath.length - 2];
+                        if (rootPath === "pm25.github.io") {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    })
+                    .map((project, key) => {
+                        let splittedPath = project.url.split("/");
+                        let path = splittedPath[splittedPath.length - 1];
+                        return { path: path };
+                    });
+
+                const paramsConfig = {
+                    "/article/:id": articleIdMap,
+                    "/:path": projectIdMap,
+                };
+
+                return new Sitemap(router)
+                    .applyParams(paramsConfig)
+                    .build("https://pm25.github.io")
+                    .save("./public/sitemap.xml");
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+}
+
+generateSitemap();
---
+++ sitemap-routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router";
+
+export default (
+    <Route>
+        <Route path="/home" />
+        <Route path="/project" />
+        <Route path="/article" />
+        <Route path="/article/:id" />
+        <Route path="/:path" />
+    </Route>
+);
