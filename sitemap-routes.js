30da0edbf0134fa10b7d8137a9018773330a1a52
PY <pyhuang97@gmail.com>
2021-02-03 22:15:47 +0800

    update sitemap.

--- sitemap-routes.js
+++ sitemap-routes.js
@@ -3,9 +3,9 @@ import { Route } from "react-router";
 
 export default (
     <Route>
-        <Route path="/home" />
-        <Route path="/project" />
-        <Route path="/article" />
+        <Route path="/#/home" />
+        <Route path="/#/project" />
+        <Route path="/#/article" />
         <Route path="/#/article/:id" />
         <Route path="/:path" />
     </Route>
