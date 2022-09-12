f588e2c892c536576c45b1021a31575204c24607
PY <pyhuang97@gmail.com>
2022-09-13 00:59:11 +0800

    remove header link and transition-duration.

--- src/project/section/header.jsx
+++ src/project/section/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <div className="header">
-            <a href="#">Projects</a>
+            <div className="button">Projects</div>
         </div>
     );
 }
